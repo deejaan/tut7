754531775ec8e6b1b551b85d2ef3b130638b75ce
dejaan <dpopovic1@etf.unsa.ba>
2019-05-06 12:08:14 +0200

    2. i 3. zadatak

--- src/ba/unsa/etf/rs/Main.java
+++ src/ba/unsa/etf/rs/Main.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rs;
 
 import java.util.function.Function;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
@@ -9,7 +11,28 @@ public class Main {
             System.out.println(fun.apply(i));
         }
     }
+    public static Integer Suma (Integer x) {
+        Integer sumaX=0;
+        while (x != 0) {
+            sumaX += x%10;
+            x /= 10;
+        }
+        return sumaX;
+    }
     public static void main(String[] args) {
-	Raspon100(x -> x*x + 2*x + 1);
+        // Zadatak 1: Raspon100(x -> x*x + 2*x + 1);
+
+
+        System.out.print("Unesite broj elemenata: ");
+        Scanner ulaz = new Scanner(System.in);
+        int n = ulaz.nextInt(); int[] niz = new int[n];
+        System.out.print("Unesite elemenate: ");
+        for (int i=0; i<n; i++) niz[i] = ulaz.nextInt();
+
+        // Zadatak 2: Arrays.sort(niz, ( x, y) -> Suma(x) - Suma(y));
+        // Zadatak 3:
+        niz = Arrays.stream(niz).boxed().sorted((x, y) -> Suma(x) - Suma(y)).mapToInt(i -> i).toArray();
+
+        for (int el : niz) System.out.print(el + " ");
     }
 }
