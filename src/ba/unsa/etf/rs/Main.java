1f471a37529c4f98b03fba922fe0e15b8643c3c3
dejaan <dpopovic1@etf.unsa.ba>
2019-05-06 11:21:05 +0200

    1. zadatak

--- src/ba/unsa/etf/rs/Main.java
+++ src/ba/unsa/etf/rs/Main.java
@@ -1,8 +1,15 @@
 package ba.unsa.etf.rs;
 
+import java.util.function.Function;
+
 public class Main {
 
+    public static void Raspon100 (Function<Double, Double> fun) {
+        for (double i=0; i<=100; i++) {
+            System.out.println(fun.apply(i));
+        }
+    }
     public static void main(String[] args) {
-	// write your code here
+	Raspon100(x -> x*x + 2*x + 1);
     }
 }
